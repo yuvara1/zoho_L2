db16449dac708af037e1421510c7035be0394f70
yuvara1 <yuvarajacb11@gmail.com>
2026-06-24 16:12:30 +0530

    number grid pattern

--- src/pattern/NumberGrid.java
+++ src/pattern/NumberGrid.java
@@ -4,9 +4,11 @@ public class NumberGrid {
     static void printGrid(int n) {
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
-
+                // if the row is even just multiply the row with n and add col + 1
                 if (row % 2 == 0)
                     System.out.print((row * n) + col + 1 + " ");
+
+                // if the row is odd just multiply the row with n and sub n with col (n - col)
                 else System.out.print((row * n) + (n - col) + " ");
             }
             System.out.println();
