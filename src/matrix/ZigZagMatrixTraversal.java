0f24e8f2dd050b62386bb2ffcf87eb82eff56c58
yuvara1 <yuvarajacb11@gmail.com>
2026-07-08 13:29:37 +0530

    SubStringSearchIn2DGrid solved

--- src/matrix/ZigZagMatrixTraversal.java
+++ src/matrix/ZigZagMatrixTraversal.java
@@ -29,7 +29,7 @@ public class ZigZagMatrixTraversal {
             }
         }
     }
-    public static void main(String[] args) {
+    static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
